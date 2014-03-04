2ef8bab271efbf3f5fd8e3990df5bf527bed9889
Jeff Horsch <jhorsch@gmail.com>
2014-03-03 18:51:21 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Horschgallery2::Application.config.secret_key_base = '1bf56743f0ac9f199c91ab08db0f26448c5809497ea97700e690d58cef12d1b9b5012e2c52734071070ed64ece012f0b81678a7d4fbbdc090187ddd530f235b3'
