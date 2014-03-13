7855c4a561005c673aeae38883033f8eac574c5a
Jeff Horsch <jhorsch@gmail.com>
2014-03-12 23:56:02 -0500

    wed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
-    @photos = @category.photos.paginate(:page => params[:page], :per_page => 12)
+    @photos = @category.photos.paginate(:page => params[:page], :per_page => 24)
 
   end
 
