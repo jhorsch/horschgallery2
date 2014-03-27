5b3b75623dd741c2a50b1f139440ce4cb67fd91e
Kori Roys <kori@koriroys.com>
2014-03-26 19:12:35 -0500

    use postgres in development

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326223820) do
+ActiveRecord::Schema.define(version: 20140326222440) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20140326223820) do
     t.text     "description"
   end
 
-  add_index "categories", ["slug"], name: "index_categories_on_slug"
+  add_index "categories", ["slug"], name: "index_categories_on_slug", using: :btree
 
   create_table "category_mats", force: true do |t|
     t.integer  "mat_id"
