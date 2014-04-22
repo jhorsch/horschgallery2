839423024aef79d41584ab73f4ff5994bb3cd352
Jeff Horsch <jhorsch@gmail.com>
2014-04-21 19:10:33 -0500

    fixed most recently viewed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,34 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_page
-  rescue_from ActiveRecord::RecordNotFound, with: :invalid_page
 
 
   def index
   end
 
   def show
-    @customer_viewed = Photo.where(main_category: @photo.main_category, is_active: true).shuffle.take(5)
-    @recently_viewed = Photo.where(is_active: true).sample(5)
-    @rotating_keywords = ['photo','print','picture','photograph','artwork','photography']
+    @customer_viewed = Photo.customer_also_viewed(@photo.main_category)
+    @rotating_keywords = Photo.rotating_keywords
+
+
+    # initalize the session hash
+    if session[:most_recent_viewed].nil?
+      session[:most_recent_viewed] = []
+    end
+    # only add unique photo ids to the hash...might want to remove though
+    unless session[:most_recent_viewed].include? params[:id]
+      session[:most_recent_viewed] << params[:id]
+    end
+    # set local variable
+    most_recent_viewed = session[:most_recent_viewed]
+    # test whether array has more than 5 photos..if so remove the first element and reset the array
+    if most_recent_viewed.count > 5
+      most_recent_viewed.shift
+      session[:most_recent_viewed] = most_recent_viewed
+    end
+    @recently_viewed = Photo.five_most_recent(most_recent_viewed)
+
+
   end
 
   def new
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,58 +1,68 @@
 class Photo < ActiveRecord::Base
-  # belongs_to :category
-  # validates :category_id, presence: true
-
+  # associations
   has_many :line_items
   has_many :category_photos
   has_many :categories, :through => :category_photos
-
   belongs_to :format
 
-  validates :is_active, :show_bw_conversion, inclusion: [true, false]
 
+  # validations
+  validates :is_active, :show_bw_conversion, inclusion: [true, false]
   validates :id_num, :title, :artist_name, :year_taken, :format_id, :desc, :rotating_keyword, :camera, :film_type, presence: true
   validates :id_num, :title, uniqueness: {:case_sensitive => false}
 
+
+  # scopes
   scope :active, -> { where(is_active: true) }
 
 
-  def super_category_title
-    self.categories.take.super_category
+  # database queries
+  def self.rotating_keywords
+    ['photo','print','picture','photograph','artwork','photography']
   end
 
-
-  def main_category_title
-    self.categories.take.main_category.title
-  end
-
-  def category_title
-
-  end
-
-  def mats
-    self.categories.take.mats
-  end
-
-  def artist
-    self.artist_name.titleize
-  end
-
-  def max_price
-    self.format.sizes.maximum('price')
-  end
-
-  def max_price_size_id
-    self.format.sizes.order('price DESC').first.try(:id)
-  end
-
-  def all_sizes
-    self.format.sizes
+  def self.customer_also_viewed(string)
+    Photo.where(main_category: string).active.shuffle.take(5)
   end
 
   def self.search(query)
     query = query.downcase
     active.where("LOWER(title) LIKE ? OR LOWER(id_num) LIKE ?",
-                "%#{query}%", "%#{query}"
-            )
+                "%#{query}%", "%#{query}")
   end
+
+  def self.five_most_recent(session_array)
+    where(is_active: true, id: session_array).order('title asc')
+  end
+
+
+  # attribute formatting
+  def super_category_title
+    categories.take.super_category
+  end
+
+  def main_category_title
+    categories.take.main_category.title
+  end
+
+  def mats
+    categories.take.mats
+  end
+
+  def artist
+    artist_name.titleize
+  end
+
+  def max_price
+    format.sizes.maximum('price')
+  end
+
+  def max_price_size_id
+    format.sizes.order('price DESC').first.try(:id)
+  end
+
+  def all_sizes
+    format.sizes
+  end
+
 end
