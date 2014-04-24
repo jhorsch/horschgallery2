ce1251397cdd25331141fb44bd1aa154d898d995
Jeff Horsch <jhorsch@gmail.com>
2014-04-24 12:40:07 -0500

    testing env vars2

--- config/environment.rb
+++ config/environment.rb
@@ -11,8 +11,10 @@ Horschgallery2::Application.initialize!
 Horschgallery2::Application.configure do
   # Send email
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: 'domain.com'}
+
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
